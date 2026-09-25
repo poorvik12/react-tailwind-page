791976f67158e76df6057ded9a4df33ba14636d2
Poorvik <poorvik009@gmail.com>
2026-09-26 01:14:39 +0530

    Changes

--- src/components/section1/RightCardContext.jsx
+++ src/components/section1/RightCardContext.jsx
@@ -8,8 +8,8 @@ const RightCardContext = (props) => {
             <h2 className='bg-white rounded-full text-2xl h-10 w-10 flex items-center justify-center'>{props.id+1}</h2>
             <div>
                 <p  className=' text-shadow-2xl text-white mb-10 text-xl'>{props.intro}</p>
-                <div className='flex justify-between'>
-                    <button className='bg-[#2457E6] text-white px-4 sm:px-5 lg:px-6 py-2 rounded-full text-sm sm:text-base whitespace-nowrap'>{props.tag} </button>
+                <div className='flex justify-between items-center w-full overflow-hidden py-1'>
+                    <button className='bg-[#2457E6] text-white px-3 sm:px-5 lg:px-6 py-1.5 sm:py-2 rounded-full text-xs sm:text-base whitespace-nowrap'>{props.tag} </button>
                 </div>
             </div>
         </div>
